b158f0f46a8fd67f7f6845e4a0a53ae0ae1a88fa
NatchaPetchsikul <natcha_pet@cmu.ac.th>
2022-11-18 01:50:25 +0700

    try to use data from database

--- src/tempChart.js
+++ src/tempChart.js
@@ -3,6 +3,9 @@ import firebaseApp from "./firebaseApp";
 
 const dbRef = ref(firebaseApp);
 var valueArray;
+var humidity = [];
+var time = [];
+
 get(child(dbRef, `/DB_Storage/TestRoom`))
   .then((snapshot) => {
     if (snapshot.exists()) {
@@ -11,18 +14,21 @@ get(child(dbRef, `/DB_Storage/TestRoom`))
       console.log(valueArray);
 
       for (var key in valueArray) {
-        // skip loop if the property is from prototype
-
         var obj = valueArray[key];
         for (var prop in obj) {
-          // skip loop if the property is from prototype
-
-          // your code
           if (prop === "humidity") {
-            console.log(prop + " = " + obj[prop]);
+            humidity.push(obj[prop]);
+          }
+          if (prop === "time") {
+            time.push(obj[prop]);
           }
         }
       }
+
+      console.log(humidity);
+      console.log(time);
+      // console.log(humidity);
+      // console.log(time);
     } else {
       console.log("No data available");
     }
@@ -30,24 +36,16 @@ get(child(dbRef, `/DB_Storage/TestRoom`))
   .catch((error) => {
     console.error(error);
   });
-
-export const TempChartData = {
+console.log(humidity);
+console.log(time);
+const TempChartData = {
   type: "line",
   data: {
-    labels: [
-      "Mercury",
-      "Venus",
-      "Earth",
-      "Mars",
-      "Jupiter",
-      "Saturn",
-      "Uranus",
-      "Neptune",
-    ],
+    labels: time,
     datasets: [
       {
         label: "Planetary Mass (relative to the Sun x 10^-6)",
-        data: [0.166, 2.081, 3.003, 0.323, 954.792, 285.886, 43.662, 51.514],
+        data: humidity,
         backgroundColor: "rgba(71, 183,132,.5)",
         borderColor: "#47b784",
         borderWidth: 3,
